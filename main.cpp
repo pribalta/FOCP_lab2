83ff6ad2c6c71f34dbb07c9ba04e5c1985542e79
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-15 12:14:08 +0200

    Find prime numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-int main() {
+#include <iostream>
 
-    
+int main() {
+    std::cout << "Please introduce a number:" << std::endl;
+
+    int number;
+    std::cin >> number;
+
+    for (int i=2; i<number; i++) {
+        if (number % i == 0) {
+            std::cout << "Your number is not prime" << std::endl;
+            return 0;
+        }
+    }
+
+    std::cout << "Your number is prime" << std::endl;
+
     return 0;
 }
