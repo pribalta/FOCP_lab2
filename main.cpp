e7a756642d0acccb51df9b8c4c293da9452d5148
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-15 12:58:27 +0200

    Fibonacci iterative
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
-int pow(int base, int power) {
-    int result = 1;
-    
-    for (int i = 0; i < power; i++) {
-        result *= base;
+int fibonacci_iterative(int index) {
+    int a = 0;
+    int b = 1;
+
+    if (index == 0 || index == 1) {
+        return index;
     }
 
-    return result;
+    for (int i = 2; i < index; i++) {
+        int tmp = a + b;
+        a = b;
+        b = tmp;
+    }
+
+    return  b;
 }
 
 int read_int() {
@@ -19,16 +26,18 @@ int read_int() {
     return n;
 }
 
-// 1 - Read base from command line
-// 2 - Read power from command line 
-// 3 - Calculate power
-// 4 - Print the result
+// 1 - Read an integer - nth element of the fibonacci sequence
+// 2 - Calculate fibonacci - iteratively
+// 2b - Calcualte fibonacci - recursively
+// 3 - Print the number
+
+// 0, 1, 1, 2, 3, 5, 8, 13, ...
+//                   ^  ^
 
 int main() {
-    int base = read_int();
-    int power = read_int();
+    int index = read_int();
 
-    int result = pow(base, power);
+    int result = fibonacci_iterative(index);
 
     std::cout << "The result is: " << result << std::endl;
 
