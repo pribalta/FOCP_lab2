54ed57cc40bdba92dcdde1702e75f8ca64293ec1
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-15 12:23:02 +0200

    Extracted function with annotations

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 
+// [return type] [name of the function] ([arguments]) ----> signature
+
+bool is_prime(int number) {
+
+    for (int i=2; i<number/2; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+//     INPUT ----> {???} ---> OUTPUT
+//     n     ----> {is prime} ----> true/false
+
 int main() {
     std::cout << "Please introduce a number:" << std::endl;
 
     int number;
     std::cin >> number;
 
-    for (int i=2; i<number; i++) {
-        if (number % i == 0) {
-            std::cout << "Your number is not prime" << std::endl;
-            return 0;
-        }
+    if (is_prime(number)) {
+        std::cout << "Your number is prime" << std::endl;
+    } else {
+        std::cout << "Your number is NOT prime" << std::endl;
     }
 
-    std::cout << "Your number is prime" << std::endl;
+
 
     return 0;
 }
